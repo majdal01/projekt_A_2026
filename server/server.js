d15919e56551722059499b2b74da235f0be106c6
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-02-24 19:12:17 +0100

    Tilføjet login route til server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs")
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server kører!");
-});
+const SECRET = "supersecretkey"
 
-app.listen(3000, () => {
-  console.log("Server kører på http://localhost:3000");
-});
+app.post("/login", async (req, res) => {
+  const { username, password} = req.body
+
+  const users = JSON.parse(fs.readFileSync(":/users.json"))
+  const user = users.find(u => u.username === username)
+
+  if (!user) {
+    return res.status(401).json({ message: "Bruger ikke fundet" })
+  }
+
+  //Midlertidig spring password-check over
+  const token = jwt.sign(
+    { id:user.id, role: user.role },
+    SECRET,
+    { expiresIn: "1h" }
+  )
+
+  res.json({ token })
+})
