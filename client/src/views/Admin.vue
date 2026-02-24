898d1a6dedc24661b034d2b0d3ea1bb2373267ae
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-02-24 10:01:15 +0100

    Tilføjet kode til Admin.vue

---
+++ client/src/views/Admin.vue
@@ -0,0 +1,28 @@
+<template>
+    <GenericCard>
+        <template #header>
+            Admin Panel
+        </template>
+
+        <template #body>
+            <p>Kun admins må se dette.</p>
+        </template>
+
+        <template #footer>
+            Systemindstillinger
+        </template>
+    </GenericCard>
+</template>
+
+<script>
+import GenericCard from '../components/GenericCard.vue';
+
+export default {
+    components: { GenericCard }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
