ed7638124d04fba48f43cbeaf499236e764dbdf4
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-02-24 19:32:52 +0100

    Tilføjet auth i server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs")
+const { authenticate, authorize } = require("./middleware/auth")
 
 const app = express();
 app.use(cors());
@@ -28,4 +29,28 @@ app.post("/login", async (req, res) => {
   )
 
   res.json({ token })
+})
+
+app.get("/users", authenticate, authorize("admin"), (req, res) => {
+  const users = JSON.parse(fs.readFileSync("./users.json"))
+  res.json(users)
+})
+
+app.put("/users/:id", authenticate, authorize("admin"), (req, res) => {
+  const { role } = req.body
+  const users = JSON.parse(fs.readFileSync("./users.json"))
+
+  const user = user.find(u => u.id == req.params.id)
+  if (!user) {
+    return res.status(404).json({ message: "User not found" })
+  }
+
+  user.role = role
+  fs.writeFileSync("./users.json", JSON.stringify(users, null, 2))
+
+  res.json({ message: "Role updated" })
+})
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
 })
