09871a34bf8b71ba533f7e1618a9f31fad0e6f2f
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-02-24 09:41:03 +0100

    Ændret mappenavne i vue

--- client/src/components/GenericCard.vue
+++ client/src/components/GenericCard.vue
@@ -1,16 +1,10 @@
 <script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+export default {
+  name: "GenericCard"
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
