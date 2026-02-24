7e6a2783c2367a707881f3e79014ce7008fb37b3
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-02-24 19:19:28 +0100

    Oprettet middleware til auth

---
+++ server/middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken")
+
+const SECRET = "supersecretkey";
+
+function authenticate(req, res, next) {
+    const header = req.headers.authorization
+
+    if (!header) {
+        return res.status(401).json({ message: "No token" })
+    }
+
+    const token = header.split(" ")[1]
+
+    try {
+        const decoded = jwt.verify(token, SECRET)
+        req.user = decoded
+        next()
+    } catch {
+        res.status(403).json({ message: "Invalid token" })
+    }
+}
+
+function authorize(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: "Access denied" })
+        }
+        next()
+    }
+}
+
+module.exports = { authenticate, authorize }
