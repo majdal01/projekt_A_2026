fe8f96753e79339fd2506f0c8f5ad4021fe17673
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-02-26 10:38:55 +0100

    Rettet semantik, tjekket script og styling på Register.vue

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -5,37 +5,49 @@
         </template>
 
         <template #body>
-            <form class="form" @submit.prevent="handleLogin">
-                <input v-model="username" placeholder="Brugernavn" />
+            <form class="form" @submit.prevent="register">
+                <label for="username">Brugernavn</label>
                 <input 
+                    id="username"
+                    v-model="username"
+                    name="username"
+                    type="text"
+                    placeholder="Brugernavn"
+                />
+                <label for="password">Adgangskode</label>
+                <input 
+                    id="password"
                     v-model="password"
+                    name="password"
                     type="password"
                     placeholder="Adgangskode"
                 />
-                <button @click="register">Opret</button>
+                <button type="submit">Opret</button>
             </form>
         </template>
         
         <template #footer>
-            <p v-if="message">{{ message }}</p>
-            <p>Har du allerede en bruger?</p>
-            <button @click="emit('show-login')">Tilbage til login</button>
+            <div class="footer">
+                <p v-if="message" aria-live="polite">{{ message }}</p>
+                <p>Har du allerede en bruger?</p>
+                <button @click="emit('show-login')">Tilbage til login</button>
+            </div>
         </template>
     </GenericCard>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref } from "vue";
 import GenericCard from "../components/GenericCard.vue";
 
-const username = ref("")
-const password = ref("")
-const message = ref("")
+const username = ref("");
+const password = ref("");
+const message = ref("");
 
-const emit = defineEmits(["show-login"])
+const emit = defineEmits(["show-login"]);
 
 async function register() {
-    console.log("Register clicked")
+    console.log("Register clicked");
     const res = await fetch("http://localhost:3000/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -43,23 +55,38 @@ async function register() {
             username: username.value, 
             password: password.value
         })
-    })
+    });
 
-    const data = await res.json()
+    const data = await res.json();
 
     if (res.ok) {
-        alert("Bruger oprettet! Log ind.")
-        emit("show-login")
+        alert("Bruger oprettet! Log ind.");
+        emit("show-login");
     } else {
-        message.value = data.message
+        message.value = data.message;
     }
-
 }
-
-
-
 </script>
 
 <style lang="scss" scoped>
+   .form {
+    display: inline-flex;
+    flex-direction: column;
+    text-align: left;
 
+    input {
+        margin-bottom: 1.5rem;
+        height: 1.5rem;
+        min-width: 20rem;
+        padding: 0.2rem;
+        font-size: 1.1rem;
+    }
+    input::placeholder {
+        font-size: 1.1rem;
+        align-self: center; 
+    }
+   }
+   .footer {
+    margin-top: 2rem;
+   }
 </style>
